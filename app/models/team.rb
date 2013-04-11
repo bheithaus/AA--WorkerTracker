ab0fb07f5f56e806cf6342f684f4fce52a28eee5
Brian Heithaus <bheithaus@gmail.com>
2013-04-11 10:10:41 -0700

    added class_name 'Employee' to two relationships in Team'

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,8 +1,8 @@
 class Team < ActiveRecord::Base
   attr_accessible :name, :supervisor_id
 
-  belongs_to :supervisor, inverse_of: :supervised_teams
+  belongs_to :supervisor, class_name: 'Employee', inverse_of: :supervised_teams
 
   has_many :team_memberships
-  has_many :members, through: :team_memberships, source: :employee_id
+  has_many :members, through: :team_memberships, class_name: 'Employee', source: :employee
 end
